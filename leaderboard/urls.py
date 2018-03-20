85112d8907ba3de6e3d7eb6394feac1d17d8e210
aburn <aburn@Earendil>
2018-03-20 22:25:25 +0100

    Added admin page and some js to add new players

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'leaderboard'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^admin$', views.AdminView.as_view(), name='admin'),
     url(r'^api/lap/new/$', views.create_lap, name='create_lap'),
     url(r'^api/player/new/$', views.create_player, name='create_player'),
 ]
