f9fdb70f5f9139221c544d6bd58be20072fbc890
Christopher Peirs <christopher.peirs@panimpex.com>
2018-03-20 20:29:29 +0100

    add api entry points to create new players and new laps

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'leaderboard'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^api/lap/new/$', views.create_lap, name='create_lap'),
+    url(r'^api/player/new/$', views.create_player, name='create_player'),
 ]
